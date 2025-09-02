f5a3b7e3f5103e03c95db8e853ce340cabc506ad
2333 <zk.amaranth@gmail.com>
2025-09-02 18:35:26 +0800

    switch to tabulator

--- instock/lib/database.py
+++ instock/lib/database.py
@@ -15,7 +15,7 @@ db_host = "home.ihulu.us"  # 数据库服务主机
 db_user = "root"  # 数据库访问用户
 db_password = "root"  # 数据库访问密码
 db_database = "instockdb"  # 数据库名称
-db_port = 33306  # 数据库服务端口
+db_port = 43306  # 数据库服务端口
 db_charset = "utf8mb4"  # 数据库字符集
 
 # 使用环境变量获得数据库,docker -e 传递
